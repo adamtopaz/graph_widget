a1345cdce8fb51f8ef87628ed41597200758fe00
adamtopaz <github@adamtopaz.com>
2024-05-09 19:49:11 -0600

    fix again

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.36"
 
-package «GraphWidget» where
+package «graph_widget» where
   -- add package configuration options here
 
 @[default_target]
