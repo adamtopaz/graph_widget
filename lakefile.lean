e8de1ddde013f6e0678f66e600d0bad6c5a4ed28
adamtopaz <github@adamtopaz.com>
2024-05-09 19:48:30 -0600

    fix name

--- lakefile.lean
+++ lakefile.lean
@@ -3,9 +3,9 @@ open Lake DSL
 
 require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.36"
 
-package «SpecGraph» where
+package «GraphWidget» where
   -- add package configuration options here
 
 @[default_target]
-lean_lib «SpecGraph» where
+lean_lib «GraphWidget» where
   -- add library configuration options here
